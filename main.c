bcbd3304c1cf459e642b2a822b9e82bd394d308b
Nordin Belkacemi <belkaceminordin@gmail.com>
2019-11-09 19:45:36 +0100

    not bad

--- main.c
+++ main.c
@@ -2,133 +2,199 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct Character {
+void feed(int *char_freq, char *filename) {
+    FILE *f = fopen(filename, "r");
+    char c;
+    int total = 0;
+    while ((c = fgetc(f)) != EOF) {
+        char_freq[c] += 1;
+        total += 1;
+    }
+    printf("total characters = %d", total);
+    fclose(f);
+}
+
+// determines how many distinct characters the text file contains.
+int character_set_size(int *char_freq) {
+    int size = 0;
+    for (int i = 0; i < 128; i++)
+        if (char_freq[i] != 0)
+            size += 1;
+    return size;
+}
+
+typedef struct HuffNode {
     char c;
     int freq;
-} Character;
+    struct HuffNode *left, *right;
+} HuffNode;
 
-typedef struct Node {
-    Character entry;
-    struct Node *next;
-} Node;
+typedef struct Queue {
+    int initsize, size;
+    HuffNode **array; // this array will always be sorted by frequency
+} Queue;
 
-Node *search(char c, Node *list) {
-    Node *p = list;
-    while (p != NULL) {
-        if (p->entry.c == c)
-            return p;
-        p = p->next;
-    }
-    return NULL;
+bool is_leaf(HuffNode *node) {
+    return node->left == NULL && node->right == NULL;
 }
 
-void add(Node *list, char c, int *size) {
-    Node *node = search(c, list);
-    if (node != NULL)
-        node->entry.freq += 1;
-    else {
-        Node *new = (Node *) malloc(sizeof(Node));
-        new->entry.c = c;
-        new->entry.freq = 1;
-        
-        Node *p = list;
-        while (p->next != NULL)
-            p = p->next;
-
-        p->next = new;
-        new->next = NULL;
-        *size += 1;
+void print_node(HuffNode *node) {
+    if (is_leaf(node)) {
+        if (node->c == '\n')
+            printf("\\n");
+        else if (node->c == '\t')
+            printf("\\t");
+        else
+            printf("%c", node->c);
+        printf("/%d\n", node->freq);
+    } else {
+        printf("%s/%d\n", "internal node", node->freq);
     }
 }
 
-void swap(Character *a, Character *b) {
-    Character tmp = *a;
+void print_queue(Queue *q) {
+    printf("\n");
+    for (int i = 0; i < q->size; i++){
+        if (is_leaf(q->array[i])) {
+            if (q->array[i]->c == '\n')
+                printf("\\n");
+            else if (q->array[i]->c == '\t')
+                printf("\\t");
+            else
+                printf("%c", q->array[i]->c);
+            printf("/%d\n", q->array[i]->freq);
+        } else {
+            printf("%s/%d\n", "internal node", q->array[i]->freq);
+        }
+    }
+    printf("\n");
+}
+
+Queue *create_queue(int initsize) {
+    Queue *q = (Queue *) malloc (sizeof(Queue));
+
+    q->size = 0;
+    q->initsize = initsize;
+    q->array = (HuffNode **) malloc (initsize * sizeof(HuffNode *));
+
+    return q;
+}
+
+// shifts all the nodes in the queue to the left by 2 starting with index 2
+void shift(Queue *q) {
+    for (int i = 2; i < q->size; i++)
+        q->array[i - 2] = q->array[i];
+}
+
+// removes first two elements from the queue
+void remove_first_two(Queue *q) {
+    shift(q);
+    q->size -= 2;
+}
+
+// extracts the two nodes with the smallest frequency (minimum nodes) and makes a new parent node
+// with a frequency equal to the sum of its two children (the minimum nodes).
+HuffNode *new_internal_node(Queue *q) {
+    HuffNode *parent = (HuffNode *) malloc (sizeof(HuffNode));
+
+    // queue is always sorted, so
+    HuffNode *left = q->array[0];
+    HuffNode *right = q->array[1];
+    remove_first_two(q);
+
+    parent->freq = left->freq + right->freq;
+    parent->left = left;
+    parent->right = right;
+
+    return parent;
+}
+
+void insert(Queue *q, HuffNode *node) {
+    if (q->size == q->initsize) {
+        fprintf(stderr, "Error: Can't add node because the queue is full.");
+        exit(1);
+    }
+
+    int insertion_idx = q->size;
+    for (int i = 0; i < q->size; i++) {
+        if (node->freq <= q->array[i]->freq) {
+            insertion_idx = i;
+            break;
+        }
+    }
+
+    for (int i = q->size - 1; i >= insertion_idx; i--)
+        q->array[i + 1] = q->array[i];
+
+    q->array[insertion_idx] = node;
+    q->size += 1;
+}
+
+void swap(HuffNode **a, HuffNode **b) {
+    HuffNode *tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-void quicksort(Character *chars, int min, int max) {
-    Character pivot = chars[(min + max) / 2];
+void quicksort(Queue *q, int min, int max) {
+    HuffNode *pivot = q->array[(min + max) / 2];
     int i = min, j = max;
     while (i <= j) {
-        while (chars[i].freq < pivot.freq)
+        while (q->array[i]->freq < pivot->freq)
             i += 1;
-        while (chars[j].freq > pivot.freq)
+        while (q->array[j]->freq > pivot->freq)
             j -= 1;
         if (i <= j) {
-            swap(chars + i, chars + j);
+            swap(&q->array[i], &q->array[j]);
             i += 1;
             j -= 1;
         }
     }
 
     if (min < j)
-        quicksort(chars, min, j);
+        quicksort(q, min, j);
     if (i < max)
-        quicksort(chars, i, max);
+        quicksort(q, i, max);
 }
 
-void fillarray(Character *array, Node *list) {
-    Node *p = list;
-    int i = 0;
-    while (p != NULL) {
-        array[i] = p->entry;
-        p = p->next;
-        i += 1;
+void huffman_reduce(Queue *q) {
+    HuffNode *internal_node = new_internal_node(q);
+    insert(q, internal_node);
+}
+
+Queue *sample(char *filename) {
+    int char_freq[128] = {0};
+    feed(char_freq, filename);
+    int size = character_set_size(char_freq);
+
+    Queue *q = create_queue(character_set_size(char_freq));
+    if (q == NULL) {
+        fprintf(stderr, "Error\n");
+        exit(1);
     }
-}
 
-bool check_duplicates(Character *chars, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (chars[i].c == chars[j].c)
-                return true;
+    for (int i = 0; i < 128; i++) {
+        if (char_freq[i] != 0) {
+            HuffNode *node = (HuffNode *) malloc (sizeof(HuffNode));
+            node->c = i;
+            node->freq = char_freq[i];
+            node->left = NULL;
+            node->right = NULL;
+            q->array[q->size] = node;
+            q->size += 1;
         }
     }
-    return false;
-}
-
-Character *read(char *filename) {
-    Node *list = (Node *) malloc(sizeof(Character));
-    list->next = NULL;
-    int size = 0;
-
-    FILE *f = fopen(filename, "r");
-    char c;
-    while ((c = fgetc(f)) != EOF) {
-        if (size == 0) {
-            list->entry.c = c;
-            list->entry.freq = 1;
-            size += 1;
-        }
-        else {
-            add(list, c, &size);
-        }
-    }
-
-    Character *chars;
-    chars = (Character *) malloc(size * sizeof(Character));
-    fillarray(chars, list);
-    printf("%s\n\n", check_duplicates(chars, size) ? "Duplicates found!" : "No duplicates found");
-    free(list);
-    quicksort(chars, 0, size - 1);
-    return chars;
-}
-
-void printchars(Character *chars) {
-    for (int i = 0; chars[i].c != '\0'; i++) {
-        if (chars[i].c == '\n')
-            printf("\\n:\t");
-        else
-            printf("%c:\t", chars[i].c);
-        printf("%d\n", chars[i].freq);
-    }
+    quicksort(q, 0, q->size - 1);
+    return q;
 }
 
 int main() {
-    Character *chars;
-    chars = read("test.txt");
-    printchars(chars);
-    free(chars);
+    Queue *q = sample("test.txt");
+    print_queue(q);
+    while (q->size != 1) {
+        huffman_reduce(q);
+    }
+    print_queue(q);
+    
     return 0;
 }
